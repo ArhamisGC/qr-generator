8fcf1adccfb79261eefe2a66bab1f9fd765e8764
hunte <hunterofseries13@gmail.com>
2024-08-12 19:51:19 +0100

    Show qr image

--- qr-generator.py
+++ qr-generator.py
@@ -1,17 +1,32 @@
+import qrcode
+import qrcode.constants
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import ImageTk, Image
 
-# Crear una ventana básica
+url = input("Input a URL:")
+
+qr = qrcode.QRCode(
+    version=3,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 5
+)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="#512da8",back_color="black")
+img.save("qrcode.png")
+
 root = tk.Tk()
-root.title("Prueba de Tkinter y Pillow")
 
-# Crear una imagen de prueba con Pillow
-image = Image.new("RGB", (100, 100), "blue")
-photo = ImageTk.PhotoImage(image)
+img_tk = ImageTk.PhotoImage(img)
 
-# Mostrar la imagen en la ventana
-label = tk.Label(root, image=photo)
+label = tk.Label(root, image=img_tk)
 label.pack()
 
-# Ejecutar la aplicación
+width,height = img.size
+
+root.geometry(f"{width}x{height}")
+
 root.mainloop()
